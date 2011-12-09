a9fbf7f06ebde2dc2f28f27e8197526a1560fd4e
usmanm <usmanm@mit.edu>
2011-12-08 20:20:26 -0800

    rm print statements

--- user/djosserv.c
+++ user/djosserv.c
@@ -190,7 +190,6 @@ process_page_req(char *buffer)
 
 	cprintf("Processing page req\n");
 	src_id = *((envid_t *) buffer);
-	cprintf("source id: %d\n", src_id);
 	buffer += sizeof(envid_t);
 
 	// Check lease map
@@ -201,17 +200,14 @@ process_page_req(char *buffer)
 
 	// Read va to copy data on. Must be page aligned.
 	va = *((uintptr_t *) buffer);
-	cprintf("va %x\n",  va);
 	buffer += sizeof(uintptr_t);
 
 	// Read perms
 	perm = *((uint32_t *) buffer);
-	cprintf("perm %x\n", perm);
 	buffer += sizeof(uint32_t);
 
 	// Read *chunk/split* id, 0 <= i <= 3 (four 1024 byte chunks)
 	i = *buffer;
-	cprintf("chunk id %d\n", i);
 	buffer++;
 
 	if (debug) {
