404febb7d85ecad7bd201caf98d1ae17756ea100
usmanm <usmanm@mit.edu>
2011-12-08 20:28:59 -0800

    copy_mem bugfix

--- kern/syscall.c
+++ kern/syscall.c
@@ -592,8 +592,9 @@ int // Only copies 1024 bytes!
 sys_copy_mem(envid_t dst_id, void* dst, void* src)
 {
 	void *addr;
+	void *mapva = (void *) ROUNDDOWN(dst, PGSIZE);
 
-	if (sys_page_map(dst_id, dst, curenv->env_id, (void *) UTEMP, 
+	if (sys_page_map(dst_id, mapva, curenv->env_id, (void *) UTEMP, 
 			 PTE_U | PTE_P| PTE_W) < 0)
 		return -E_INVAL;
 
