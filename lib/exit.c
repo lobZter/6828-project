9301141fbbd2fcc9c3fa1ee341d513713926898a
Usman Masood <usmanm@mit.edu>
2011-12-12 01:31:27 -0500

    exit gracefully v2

--- lib/exit.c
+++ lib/exit.c
@@ -4,14 +4,14 @@
 void
 exit(void)
 {
-	close_all();
-
 	// Check if is leased task and completed
+	cprintf("Boom\n");
 	if (thisenv->env_alien) {
 		cprintf("EXIT %x\n", thisenv->env_id);
 		sys_lease_complete();
 	}
 
+	close_all();
 	sys_env_destroy(0);
 }
 
