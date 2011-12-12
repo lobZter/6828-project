99ca765b7243072e6644e286db4ea450e5497638
Usman Masood <usmanm@mit.edu>
2011-12-12 18:06:40 -0500

    hello

--- user/hello.c
+++ user/hello.c
@@ -6,15 +6,12 @@ umain(int argc, char **argv)
 	int id;
 	int val;
 
-	sys_yield();
-	sys_yield();
-
 	id = fork();
 	if (id == 0) {
 		sys_migrate(&thisenv);
 		cprintf("hello world! i am child environment %08x\n", 
 			thisenv->env_id);
-		
+/*		
 		id = fork();
 		if (!id) {
 			while (sys_migrate(&thisenv) != 0) {
@@ -24,7 +21,7 @@ umain(int argc, char **argv)
 				"%08x\n", 
 				thisenv->env_id);
 		}
-
+*/
 	}
 	else {
 		cprintf("hello world! i am parent environment %08x\n", 
