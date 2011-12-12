af84c5bb5c69a5c09a11389f522294611bc64c02
Usman Masood <usmanm@mit.edu>
2011-12-12 02:39:18 -0500

    exit

--- lib/exit.c
+++ lib/exit.c
@@ -5,12 +5,11 @@ void
 exit(void)
 {
 	// Check if is leased task and completed
-/*	cprintf("Boom %x\n", thisenv->env_id);
 	if (thisenv->env_alien) {
 		cprintf("EXIT %x\n", thisenv->env_id);
 		sys_lease_complete();
 	}
-*/
+
 	close_all();
 	sys_env_destroy(0);
 }
