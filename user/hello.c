5fbf87e4080eab70d58957fdf349ac4281921f3d
Usman Masood <usmanm@mit.edu>
2011-12-12 14:57:22 -0500

    hello

--- user/hello.c
+++ user/hello.c
@@ -31,24 +31,12 @@ umain(int argc, char **argv)
 			thisenv->env_id);
 		val = ipc_recv(NULL, NULL, NULL);
 		cprintf("===> parent sent me %x\n", val);
-
-		ipc_send(thisenv->env_parent_id, 02200, NULL, 0x0);
-		cprintf("===> sending parent 200\n");
-		
-		val = ipc_recv(NULL, NULL, NULL);
-                cprintf("===> parent sent me %x\n", val);
 	}
 	else {
 		cprintf("===> hello world! i am parent environment %08x\n", 
 			thisenv->env_id);
 		ipc_send(id, 0x100, NULL, 0x0);
 		cprintf("===> send child 100\n");
-		
-		val = ipc_recv(NULL, NULL, NULL);
-		cprintf("===> child sent me %x\n", val);
-
-		ipc_send(id, 0x300, NULL, 0x0);
-		cprintf("===> send child 300\n");
 	}
 
 }
