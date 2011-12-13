fcdfc787147c76422ef3d28a566e210da11ff704
Usman Masood <usmanm@mit.edu>
2011-12-13 16:18:54 -0500

    ipc v1 server bug

--- user/djosserv.c
+++ user/djosserv.c
@@ -355,6 +355,7 @@ process_ipc_start(char *buffer)
 	}
 	
 	// FIX syscall api to ensure ipc souce reflected as packet.pkt_src
+	cprintf("sending ipc to dest %x\n", dst);
 	r = sys_ipc_try_send(dst, packet.pkt_val, (void *) packet.pkt_va, 
 			     packet.pkt_perm);
 
