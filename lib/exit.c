8a28f715d7f9a9b106a5ced2fc28043eca4acc7a
Usman Masood <usmanm@mit.edu>
2011-12-14 00:36:44 -0500

    2 level ipc debug

--- lib/exit.c
+++ lib/exit.c
@@ -7,7 +7,7 @@ exit(void)
 	// Check if is leased task and completed
 	if (thisenv->env_alien) {
 		cprintf("fucking a %x\n", thisenv->env_id);
-		while (!sys_lease_complete()) {
+		while (sys_lease_complete() < 0) {
 			cprintf("boom\n");
 		}
 		cprintf("lalalala\n");
