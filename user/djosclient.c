8223d8287a8c944b3646f8cc2f6b80878e3e1d9c
Usman Masood <usmanm@mit.edu>
2011-12-13 22:00:37 -0500

    adding support for server_map: debug

--- user/djosclient.c
+++ user/djosclient.c
@@ -502,7 +502,7 @@ send_ipc_start(struct ipc_pkt *packet, int sid)
 	memmove((void *) (buffer + 1), 
 		(void *) packet, sizeof(struct ipc_pkt));
 
-	if (debug){
+//	if (debug){
 		cprintf("Sending IPC Start: \n"
 			"  src_id: %x\n"
 			"  dst_id: %x\n"
@@ -510,7 +510,7 @@ send_ipc_start(struct ipc_pkt *packet, int sid)
 			"  toalien?: %d\n",
 			packet->pkt_src, packet->pkt_dst, packet->pkt_val,
 			packet->pkt_toalien);
-	}
+//	}
 	
 	return send_buff(buffer, IPC_START_SZ, sid);
 }
