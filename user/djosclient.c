b80bc11c8dcc5a945298b5cfc437b5c3abf02f89
Usman Masood <usmanm@mit.edu>
2011-12-14 05:41:19 -0500

    ipc page v0 - pg code only

--- user/djosclient.c
+++ user/djosclient.c
@@ -566,7 +566,7 @@ send_ipc_req(struct ipc_pkt *packet, int sid)
 		}
 
 		if (r < 0) continue;
-
+/*
 		// Send page
 		if (packet->pkt_va != UTOP) {
 			r = send_page_req(packet->pkt_dst, packet->pkt_va, 
@@ -581,7 +581,7 @@ send_ipc_req(struct ipc_pkt *packet, int sid)
 			
 			if (r < 0) continue;
 		}
-
+*/
 		// Send done ipc
 		r = send_ipc_done(packet, sid);
 		switch (r) {
