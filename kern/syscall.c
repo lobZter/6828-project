ffad898e27ad21932f79fb15d2e87574de3753ae
Usman Masood <usmanm@mit.edu>
2011-12-14 08:33:23 -0500

    try send suspend:debug

--- kern/syscall.c
+++ kern/syscall.c
@@ -443,7 +443,7 @@ djos:
         sys_page_alloc(curenv->env_id, (void *) IPCSND, PTE_U|PTE_P|PTE_W);
 
         // Put data in temp page
-	if (curenv->type == ENV_TYPE_JDOSS) {
+	if (curenv->env_type == ENV_TYPE_JDOSS) {
 		rcv->env_ipc_from = *((envid_t *)(DJOSTEMP)); // sender
 	}
 	else {
