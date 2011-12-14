e206210a1afcc66e5e53c949f0254b9a5ea588e2
Usman Masood <usmanm@mit.edu>
2011-12-14 07:04:58 -0500

    adding ip.h

--- user/ip.h
+++ user/ip.h
@@ -1,7 +1,7 @@
 #ifndef JOS_USER_IP_H
 #define JOS_USER_IP_H
 
-#define SERVIP 0x12bb0049 // Server ip
-#define SERVPORT 26591 // Server port
+#define CLIENTIP 0x12bb0049 // Server ip
+#define CLIENTPORT 26591 // Server port
 
 #endif /* JOS_USER_IP_H */
