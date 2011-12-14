2333ac0a2982ef0d928e80c2b72ce8d75cf25147
Usman Masood <usmanm@mit.edu>
2011-12-14 15:37:32 -0500

    ipc1024data v0:debug

--- kern/syscall.c
+++ kern/syscall.c
@@ -672,13 +672,12 @@ sys_copy_mem(envid_t env_id, void* addr, void* buf, int perm, bool frombuf)
 	if (sys_page_map(env_id, pgva, curenv->env_id, (void *) UTEMP, 
 			 perm) < 0) 
 		return -E_INVAL;
-	cprintf("I am here\n");
 
 	if (frombuf) {
 		memmove((void *) (UTEMP + PGOFF(addr)), buf, 1024);
 	}
 	else {
-		cprintf("BOOM\n");
+		cprintf("buf %p, addr %x\n", buf, addr);
 		memmove(buf, (void *) (UTEMP + PGOFF(addr)), 1024);
 	}
 	cprintf("lol\n");
