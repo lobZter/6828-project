299edabd0ace004decc9995991310e6867b81bd8
Usman Masood <usmanm@mit.edu>
2011-12-13 22:02:47 -0500

    adding support for server_map: debug

--- user/djosserv.c
+++ user/djosserv.c
@@ -331,7 +331,7 @@ process_ipc_start(char *buffer)
 
 	struct ipc_pkt packet = *((struct ipc_pkt *) buffer);
 
-	if (debug) {
+//	if (debug) {
 		cprintf("New IPC packet: \n"
 			"  src_id: %x\n"
 			"  dst_id: %x\n"
@@ -339,7 +339,7 @@ process_ipc_start(char *buffer)
 			"  val: %d\n",
 			packet.pkt_src, packet.pkt_dst, packet.pkt_toalien, 
 			packet.pkt_val);
-	}
+//	}
 
 	// IPC to an alien env
 	if (packet.pkt_toalien) {
