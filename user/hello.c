9b6442269574508e8a3a69df9ee5b2c254318756
Usman Masood <usmanm@mit.edu>
2011-12-13 23:30:05 -0500

    2 level ipc debug

--- user/hello.c
+++ user/hello.c
@@ -3,52 +3,23 @@
 void
 umain(int argc, char **argv)
 {
-	envid_t id;
-	int r;
-
-	id = fork();
+	int id, val;
 	
-	if (!id) {
-		while((r = sys_migrate(&thisenv)) < 0) {
-			sys_yield();
-		}
+	id = fork();
 
-		id = fork();
-		
-		if (!id) {	
-			while ((r = sys_migrate(&thisenv)) < 0) {
-				sys_yield();
-			}
-		
-			cprintf("===> Hello World! I am grand child at %x.\n", 
-				thisenv->env_id);
-
-			r = 0x100;
-			ipc_send(thisenv->env_parent_id, r, NULL, 0);
-			cprintf("===> Sent %x to %x from %x.\n", r, 
-				thisenv->env_parent_id, thisenv->env_id);
-		}
-		else {
-			cprintf("===> Hello World! I am child at %x.\n", 
-				thisenv->env_id);
-			
-			r = ipc_recv(&id, NULL, NULL);
-			cprintf("===> Got %x from %x to %x.\n", r, id,
-				thisenv->env_id);
-
-			r = r + 0x100;
-			ipc_send(thisenv->env_parent_id, r, NULL, 0);
-			cprintf("===> Sent %x to %x from %x.\n", r,
-				thisenv->env_parent_id, thisenv->env_id);
-		}
-	}
-	else {    
-		cprintf("===> Hello World! I am parent at %x.\n", 
-			thisenv->env_id);
-
-		r = ipc_recv(&id, NULL, NULL);
-		cprintf("===> Got %x from %x to %x.\n", r, id,
-			thisenv->env_id);
+	while (sys_migrate(&thisenv) < 0) {
+		sys_yield();
 	}
 
+	if (id) {
+		cprintf("===> Hello World! I am child.\n");
+		val = ipc_recv(NULL, NULL, NULL);
+		cprintf("===> I got %x.\n", val);
+	}
+	else {
+		cprintf("===> Hello World! I am parent.\n");
+		val = 0x100;
+		ipc_send(id, val, NULL, 0);
+		cprintf("===> I sent %x.\n", val);
+	}
 }
